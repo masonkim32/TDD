2a82838060043e346c1c9adab5e569223a2493c1
mason <masonkim32@gmail.com>
2020-02-14 14:41:01 +0100

    Adjust model so that items are associated with different lists

--- django/lists/views.py
+++ django/lists/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import ListView, TemplateView, CreateView
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class Homepage(TemplateView):
@@ -17,8 +17,9 @@ class TodoList(ListView):
 class NewList(View):
 
     def post(self, request):
+        new_list = List.objects.create()
         new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
+        Item.objects.create(text=new_item_text, list=new_list)
 
         list_url = reverse('todo_list')
         return redirect(to=list_url)
