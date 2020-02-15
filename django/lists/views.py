7923947359eefd235da5082005a498d80c30ac29
mason <masonkim32@gmail.com>
2020-02-15 09:14:23 +0100

    New URL + view for adding to existing lists. FT passes.

--- django/lists/views.py
+++ django/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
-from django.views.generic import ListView, TemplateView, CreateView
+from django.views.generic import ListView, TemplateView, CreateView, DetailView
 
 from lists.models import Item, List
 
@@ -13,6 +13,13 @@ class Homepage(TemplateView):
 class TodoList(ListView):
     model = Item
 
+    def get_context_data(self, **kwargs):
+        list_ = List.objects.get(id=self.kwargs['pk'])
+        items = Item.objects.filter(list=list_)
+        context_data = super().get_context_data(**kwargs)
+        context_data.update({'items': items, 'list': list_})
+        return context_data
+
 
 class NewList(View):
 
@@ -21,5 +28,16 @@ class NewList(View):
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text, list=new_list)
 
-        list_url = reverse('todo_list')
+        list_url = reverse('todo_list', kwargs={'pk': new_list.id})
+        return redirect(to=list_url)
+
+
+class AddItem(View):
+
+    def post(self, request, **kwargs):
+        list_ = List.objects.get(id=self.kwargs['pk'])
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text, list=list_)
+
+        list_url = reverse('todo_list', kwargs={'pk': list_.id})
         return redirect(to=list_url)
--- django/superlists/urls.py
+++ django/superlists/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Homepage.as_view(), name='home'),
     path('lists/new', views.NewList.as_view(), name='new_todo_list'),
-    path('lists/the-only-list-in-the-world/',
-         views.TodoList.as_view(),
-         name='todo_list'),
+    path('lists/<int:pk>/', views.TodoList.as_view(), name='todo_list'),
+    path('lists/<int:pk>/add_item', views.AddItem.as_view(), name='add_item'),
 ]
